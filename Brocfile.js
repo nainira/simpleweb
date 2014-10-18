688f9f1a70e4d21a28e047c9cd3dc05e8fe22a33
Ryan Kim <nainirajk@gmail.com>
2014-10-19 02:04:42 +0900

    bower install fastclick and add it to brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -24,4 +24,5 @@ var app = new EmberApp({
 // along with the exports of each module as its value.
 
 // module.exports = app.toTree();
+app.import('bower_components/fastclick/lib/fastclick.js');
 module.exports = mergeTrees([app.toTree()]);
